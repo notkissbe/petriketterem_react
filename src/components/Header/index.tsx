496c7faed79b3ad580cea0e6cdf6a1073dc6a426
notkissbe <bencetel1@gmail.com>
2024-02-25 19:29:01 +0100

    Header Spacing fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,11 +8,11 @@ export function Header(){
     return(
         <header>
             <nav className="navbar bg-dark">
-                <Link className="nav-brand ms-3" to="/">
+                <Link className="nav-brand mx-3" to="/">
                     <img id="logo" src="src/images/restaurantlogo.png" alt="logo"/>
                 </Link>
-                <a className="nav-link ms-3" href='#elerhetosegek'>Elérhetőségek</a>
-                <Link className="nav-link ms-3" to="/etlap" id="etlap">Étlap</Link>
+                <a className="nav-link mx-3" href='#elerhetosegek'>Elérhetőségek</a>
+                <Link className="nav-link mx-3" to="/etlap" id="etlap">Étlap</Link>
       </nav>
     </header>
     )
