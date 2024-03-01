6986c574037571102cfc77a1261f1588207f7e2a
notkissbe <bencetel1@gmail.com>
2024-03-01 10:37:05 +0100

    a Co-authored-by: SchBenedek <SchBenedek@users.noreply.github.com>

--- src/Pages/Rendelés/rendeles.tsx
+++ src/Pages/Rendelés/rendeles.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header/index'
 import { Footer } from '../../components/Footer/index'
+import { Kosar } from '../../components/kosar/kosar'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
 import './rendeles.css'
 import '../pages_shared.css'
@@ -9,11 +10,36 @@ export function Rendeles() {
   return (
     <div>
       <Header></Header>
-        <div id="rendelesContent" className="container">
-            <h3>Mai ajánlatunk:</h3>
-            
-            
+      <div id="rendelesContent" className="container">
+        <div className='row'>
+          <div className='col border border-danger rounded'>
+            <nav className="navbar navbar-light bg-dark">
+              <form className="container-fluid justify-content-start">
+                <button className="btn btn-outline-light mx-4 btn-lg" type="button">Előétel</button>
+                <button className="btn btn-outline-light mx-4 btn-lg" type="button">Levesek</button>
+                <button className="btn btn-outline-light mx-4 btn-lg" type="button">Főételek</button>
+                <button className="btn btn-outline-light mx-4 btn-lg" type="button">Köretek</button>
+                <button className="btn btn-outline-light mx-4 btn-lg" type="button">Desszertek</button>
+                <button className="btn btn-outline-light mx-4 btn-lg" type="button">Italok</button>
+              </form>
+            </nav>
+
+
+
+          <div id='MenuBox'>
+          </div>
+
+          </div>
+
+
+
+          <div className='col-2'>
+            <Kosar />
+          </div>
+
         </div>
+
+      </div>
       <Footer></Footer>
     </div>
   )
---
+++ src/components/kosar/kosar.tsx
@@ -0,0 +1,13 @@
+import {} from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../index.css'
+import './index.css'
+
+export function Kosar(){
+    return(
+        <div className='border border-3 rounded'>
+            <h3 className='text-center'>Kosár:</h3>
+            <hr />
+        </div>
+    )
+}
