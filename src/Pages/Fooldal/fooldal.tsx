c2113a78968c9b9e3900515fd980c3f8b642e35a
notkissbe <bencetel1@gmail.com>
2024-03-15 13:47:30 +0100

    document nesting validate fixed

--- src/Pages/Fooldal/fooldal.tsx
+++ src/Pages/Fooldal/fooldal.tsx
@@ -15,8 +15,10 @@ export function Fooldal() {
         <div id="cim" className='border-3 border-top border-bottom border-light'>
           <h1 className="display-4">PETRIK ÉTTEREM</h1>
           <p className="lead">
-            Üdvözöljük Önöket a Petrik Étteremben, ahol az ízek és élmények találkoznak egy harmonikus kulináris utazás során!<hr />Étteremünk büszkén kínálja vendégeinknek a minőségi alapanyagokból készült, kreatív és ínycsiklandó ételeket, melyeket a szenvedélyes szakácsaink készítenek el szeretettel.
+            Üdvözöljük Önöket a Petrik Étteremben, ahol az ízek és élmények találkoznak egy harmonikus kulináris utazás során!
           </p>
+          <hr />
+          <p className='lead'>Étteremünk büszkén kínálja vendégeinknek a minőségi alapanyagokból készült, kreatív és ínycsiklandó ételeket, melyeket a szenvedélyes szakácsaink készítenek el szeretettel.</p>
         </div>
         <div className='rendeles_div border-3 border-top border-bottom border-light'>
           <Link to={"/rendeles"}>
